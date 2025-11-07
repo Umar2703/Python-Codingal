be936505ee28a8663d2577f3ec7e80759ba2f91f
Umar2703 <Farhanakhan2427@gmail.com>
2025-11-07 00:29:52 -0800

    Lesson 1

---
+++ Lesson 1/printstatements.py
@@ -0,0 +1,8 @@
+# printing a greeting message
+print("Hello Chat")
+# printing my name
+print("My name is Umar")
+# printing a funfact about myself
+print("I love playing football")
+# perform a calculation
+print("The sum of 6+7872193",6+7872193)

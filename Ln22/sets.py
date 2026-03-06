69133db2c6c445188dd77d1165110a40df99aa81
Umar2703 <Farhanakhan2427@gmail.com>
2026-03-06 11:25:00 +0300

    Ln 22

---
+++ Ln22/sets.py
@@ -0,0 +1,13 @@
+set1={1,2,3}
+print(set1)
+set2={1.0,"HEllo",(1,2,3)}
+print(set2)
+set3={1,2,3,4,3,2}
+set4=set([1,2,3,2])
+print(set4,"\n")
+set5=set([0,1,2,3,4,5])
+print("Original set:")
+print(set5)
+set5.pop()
+print("After remmoving the first element from the said set:")
+print(set5,"\n")
